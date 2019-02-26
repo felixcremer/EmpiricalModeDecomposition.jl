6becb6260a6895d57215b2818f9d39018513ced9
Felix Cremer <felix.cremer@uni-jena.de>
2019-02-26 16:59:36 +0100

    Add plotting function for imf uncertainty.

--- src/bootstrap.jl
+++ src/bootstrap.jl
@@ -1,4 +1,5 @@
 using TimeseriesSurrogates
+using Plots
 # Idea is, that I can set a function and a number of bootstraps with surrogate method
 # And the bootstrap function (or macro) redos the function with the result replaced by the surrogate of the result.
 
@@ -19,22 +20,38 @@ end
 
 
 function plot_surr(dates, org, surrs)
-    pyplot()
     surr25 = zero(org)
     surr75 = zero(org)
+    surr05 = zero(org)
+    surr95 = zero(org)
     surrmed = zero(org)
     for i in eachindex(org)
        surr75[i] = quantile([surr[i] for surr in surrs], .75)
+       surr05[i] = quantile([surr[i] for surr in surrs], .05)
        surr25[i] = quantile([surr[i] for surr in surrs], .25)
+       surr95[i] = quantile([surr[i] for surr in surrs], .95)
        surrmed[i] = median([surr[i] for surr in surrs])
     end
-    plot(dates, surr25, fill=(surr75,:orange), line=nothing)
-    plot!(dates, org, color=:blue)
-    plot!(dates, surrmed, color=:red)
-    plot!(dates, surr25)
-    plot!(dates, surr75)
-
+    #p = plot(dates, surr25, fillrange=surr75, color=:orange, line=nothing, label="Uncertainty box")
+    p = plot(dates, surrs, color=:grey, alpha=0.3, label="")
+    plot!(p, dates, org, color=:blue, label="Original Imf" )
+    plot!(p, dates, surrmed, color=:red, label="Median Imf")
+    plot!(p, dates, surr05, label="5th Percentile IMF")
+    plot!(p, dates, surr95, label="95th Percentile IMF")
+    p
     #return surr25, surr75
+end
+
+function plot_ceemd_uncert(dates, ts, xs)
+    imfplots = []
+    for i in 1:7
+        org, surr = bootstrap(ts, randomshuffle, y->ceemd(y, xs,num_imfs=i)[end-1],200)
+        p = plot_surr(dates, org, surr)
+        push!(imfplots,p)
+    end
+    p_org = plot(dates, ts)
+    fullplot = plot(p_org, imfplots..., layout=(4,2))
+    fullplot
 end
 
 macro bootstrap(nstraps, surrogate_fun, ex)
@@ -42,6 +59,7 @@ macro bootstrap(nstraps, surrogate_fun, ex)
         $(esc(ex))
     end)
 end
+
 export bootstrap
 
 deref(x) = x
