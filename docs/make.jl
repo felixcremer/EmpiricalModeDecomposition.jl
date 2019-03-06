580a089fd2dc19fc0b64a9415ad532f0e070a0f7
Felix Cremer <felix.cremer@uni-jena.de>
2019-03-06 14:28:41 +0100

    Set correct repo url in deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -14,5 +14,5 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "<repository url>"
+    repo = "github.com/felixcremer/EmpiricalModeDecomposition"
 )
