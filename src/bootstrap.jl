c28c8d2edf0c30d897e78cd96ec46321a6806030
Felix Cremer <felix.cremer@uni-jena.de>
2019-03-19 11:26:25 +0100

    Seperate metrics generation from plotting

--- src/bootstrap.jl
+++ src/bootstrap.jl
@@ -1,5 +1,6 @@
 using TimeseriesSurrogates
 using Plots
+using LibEEMD
 # Idea is, that I can set a function and a number of bootstraps with surrogate method
 # And the bootstrap function (or macro) redos the function with the result replaced by the surrogate of the result.
 
@@ -18,20 +19,35 @@ function bootstrap(ts, surrfun, fun, ntimes=10)
     return org, surrogates
 end
 
+function surrmetrics(ts, x)
+    surrmetrics(ts, x, y->ceemdan(y, 2)[:,end-1])
+end
 
-function plot_surr(dates, org, surrs)
-    surr25 = zero(org)
-    surr75 = zero(org)
-    surr05 = zero(org)
-    surr95 = zero(org)
-    surrmed = zero(org)
-    for i in eachindex(org)
+function surrmetrics(ts, x, fun)
+    org, surr = bootstrap(ts, iaaft, fun,200)
+    surr, surrmetrics(surr)
+end
+
+function surrmetrics(surrs)
+    @show size(surrs[1])
+    @show typeof(surrs[1])
+    surr25 = zero(surrs[1])
+    surr75 = zero(surrs[1])
+    surr05 = zero(surrs[1])
+    surr95 = zero(surrs[1])
+    surrmed = zero(surrs[1])
+    for i in eachindex(surrs[1])
        surr75[i] = quantile([surr[i] for surr in surrs], .75)
        surr05[i] = quantile([surr[i] for surr in surrs], .05)
        surr25[i] = quantile([surr[i] for surr in surrs], .25)
        surr95[i] = quantile([surr[i] for surr in surrs], .95)
        surrmed[i] = median([surr[i] for surr in surrs])
     end
+    return surr05, surr25, surrmed, surr75,surr95
+end
+
+function plot_surr(dates, org, surrs)
+    surr05, surr25,surrmed, surr75, surr95 = surrmetrics(org, surrs)
     #p = plot(dates, surr25, fillrange=surr75, color=:orange, line=nothing, label="Uncertainty box")
     p = plot(dates, surrs, color=:grey, alpha=0.3, label="")
     plot!(p, dates, org, color=:blue, label="")#, label="Original Imf" )
@@ -45,7 +61,7 @@ end
 function plot_ceemd_uncert(dates, ts, xs)
     imfplots = []
     for i in 1:5
-        org, surr = bootstrap(ts, iaaft, y->ceemd(y, xs,num_imfs=i, β=0.2)[end-1],200)
+        org, surr = bootstrap(ts, iaaft, y->ceemdan(y, i+1)[:,end-1],200)
         p = plot_surr(dates, org, surr)
         push!(imfplots,p)
     end
@@ -54,6 +70,7 @@ function plot_ceemd_uncert(dates, ts, xs)
     fullplot
 end
 
+#=
 macro bootstrap(nstraps, surrogate_fun, ex)
     :(for i=1:$(esc(nstraps))
         $(esc(ex))
@@ -77,3 +94,4 @@ macro bootstrap(ntimes, surr, expr)
         zip(mu, s) |> collect |> deref
     end
 end
+=#
