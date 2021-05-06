908586ad96b879ec5c5d9a1bbb33da07e91bbb5f
Hongyang Zhou <hyzhou@umich.edu>
2021-05-06 12:32:49 +0300

    No need for HaltingIterable for the new sift

--- src/utils.jl
+++ src/utils.jl
@@ -73,27 +73,3 @@ function interpolate(knotxvals::Vector, knotyvals::Vector, predictxvals::Abstrac
     #@show spl, predictxvals
     Dierckx.evaluate(spl,predictxvals)
 end
-
-
-struct HaltingIterable{I, F}
-    iter::I
-    fun::F
-end
-
-function iterate(iter::HaltingIterable)
-    next = iterate(iter.iter)
-    return dispatch(iter, next)
-end
-
-function iterate(iter::HaltingIterable, (instruction, state))
-    if instruction == :halt return nothing end
-    next = iterate(iter.iter, state)
-    return dispatch(iter, next)
-end
-
-function dispatch(iter::HaltingIterable, next)
-    if next === nothing return nothing end
-    return next[1], (iter.fun(next[1]) ? :halt : :continue, next[2])
-end
-
-halt(iter::I, fun::F) where {I,F} = HaltingIterable{I,F}(iter, fun)
